ae900536b59dcc5b6ae0bb191be0d5e0554e24ac
Alexis Chatelain <77494628+AlexisChatelain@users.noreply.github.com>
2021-01-22 17:44:19 +0100

    Remplissage aléatoire (+ correction d'un bug)
    correction d'un bug sur le remplissage répété de la grille

--- game.js
+++ game.js
@@ -26,6 +26,7 @@ c.addEventListener('click', function(event) {
     var x = Math.floor(event.offsetX / cellSize);
     var y = Math.floor(event.offsetY / cellSize);
     console.log("X:"+x+" Y: "+ y);
+    console.log(grid[x][y]);
     if (grid[x][y].statut==1)
         grid[x][y].statut=0;
     else
@@ -92,10 +93,18 @@ c.addEventListener('click', function(event) {
         this.dessiner();
     }
     dessiner() {
-        if (this._statut==0){
-            ctx.clearRect(this._x * cellSize, this._y*cellSize, cellSize, cellSize);           
-        }else{                    
-            ctx.fillStyle = 'rgba(0, 0, 0, '+ this._tempsVie / 4  + ')';            
+       ctx.clearRect(this._x * cellSize, this._y*cellSize, cellSize, cellSize);  
+       if (this._statut!=0){
+            /*        
+            if (this._tempsVie==1)                
+                ctx.fillStyle = 'rgba(190, 190, 190)';            
+            else if (this._tempsVie==2)                
+            ctx.fillStyle = 'rgba(133, 1333, 133)'; 
+            else if (this._tempsVie==3)                
+                ctx.fillStyle = 'rgba(79, 79, 79)'; 
+            else               */
+            ctx.fillStyle = 'rgba(0, 0, 0, ' + this._tempsVie / 4 + ')'; 
+
             ctx.fillRect(this._x * cellSize, this._y*cellSize, cellSize, cellSize); 
         }
         ctx.stroke();    
